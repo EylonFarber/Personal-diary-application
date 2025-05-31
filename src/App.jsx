7392faf7117898787066f59cb1319f67821e2399
Eylon <eylonfar@gmail.com>
2025-05-31 16:02:45 +0200

    some changes to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import ListDisplay from './components/Listdisplay/ListDisplay'
 import SideBar from './components/HeaderAndSideBar/SideBar'
 import EntryDisplay from './components/EntryDisplay'
 import NewEntryForm from './components/NewEntryForm'
+import SortButton from './components/Listdisplay/SortButton'
 
 function App() {
 
   return (
     <div className='h-screen'>
-      {/* <Header/>
-      <div className='flex h-full  md:justify-center'>
+      <Header/>
+      <div className='flex h-full  md:justify-center relative'>
         <div className='max-md:hidden h-full'>
           <SideBar/>
         </div>
+          <SortButton/>
           <ListDisplay/> 
-      </div> */}
-      {/* <EntryDisplay/> */}
-      <NewEntryForm/>
+      </div>
+      {/* <EntryDisplay/> 
+      <NewEntryForm/> */}
     </div>
   )
 }
--- src/components/Listdisplay/ListDisplay.jsx
+++ src/components/Listdisplay/ListDisplay.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react'
 import EntryCard from './EntryCard'
-import SortButton from './SortButton'
 
 export default function ListDisplay() {
 
-  const [entryArray, setEntryArray] = useState([1,1,1,1,])
+  const [entryArray, setEntryArray] = useState([1,1,1,1,1,1,1,1,1,1,])
   return (
-<div className='w-full flex items-center justify-center'>
+<div className='w-full flex items-center justify-center md:w-[calc(100vw-150px)]'>
   {entryArray.length < 1 ? 
   <div className=''>
     <div className="badge badge-xl">You have no entries yet</div>
   </div>:
-    <div className='md:px-5 w-full flex justify-end '>
-      <SortButton/>
-      <div className='md:grid gap-4 md:grid-cols-3 md:gap-12 align-center pl-[150px] md:h-[calc(100vh-108px)] pt-[120px]'>
-      {entryArray.map(entry => entry = <EntryCard/>)}
+   
+    <div className='absolute top-[108px] md:left-[150px]'>
+      <div className='flex flex-col justify-center md:grid gap-4 md:grid-cols-3 align-center my-4 md:px-4'>
+        {entryArray.map(entry => entry = <EntryCard/>)}
+      </div>
     </div>
-</div>
-  }
+}
 </div>
   )
 }
